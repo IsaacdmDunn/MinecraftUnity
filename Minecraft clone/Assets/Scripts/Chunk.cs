be6a8a2366733f92b3c4f3c6417bba79e4822b3b
IsaacDunn <55945831+IsaacDunn@users.noreply.github.com>
2020-07-19 18:20:37 +0100

    V0.1 Cube mesh generation

---
+++ Minecraft clone/Assets/Scripts/Chunk.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> UVs = new List<Vector2>();
+
+    public void Start()
+    {
+        AddVoxelDataToChunk(transform.position);
+        GenerateMesh();
+    }
+
+    void AddVoxelDataToChunk(Vector3 pos)
+    {
+        for (int p = 0; p < 6; p++)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                int triangleIndex = VoxelData.voxelTris[p, i];
+                vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
+                triangles.Add(vertexIndex);
+                UVs.Add(VoxelData.voxelUVs[i]);
+
+                vertexIndex++;
+            }
+        }
+    }
+
+    void GenerateMesh()
+    {
+        //creates mesh
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = UVs.ToArray();
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+}
---
+++ Minecraft clone/Assets/Scripts/VoxelData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelData
+{
+    //creates an array of vertices for a cube
+    public static readonly Vector3[] voxelVerts = new Vector3[8]
+    {
+        new Vector3(0f, 0f, 0f),
+        new Vector3(1f, 0f, 0f),
+        new Vector3(1f, 1f, 0f),
+        new Vector3(0f, 1f, 0f),
+        new Vector3(0f, 0f, 1f),
+        new Vector3(1f, 0f, 1f),
+        new Vector3(1f, 1f, 1f),
+        new Vector3(0f, 01, 1f)
+    };
+    //creates an array of tris for a cube
+    public static readonly int[,] voxelTris = new int[6,6]{
+        { 0,3,1,1,3,2 }, //back
+        { 5,6,4,4,6,7 }, //front face
+        { 3,7,2,2,7,6 }, //top face
+        { 1,5,0,0,5,4 }, //bottom face
+        { 4,7,0,0,7,3 }, //left face
+        { 1,2,5,5,2,6 } //right face
+    };
+
+    //creates an array of uv co-ords
+    public static readonly Vector2[] voxelUVs = new Vector2[6]
+    {
+        new Vector2(0f, 0f),
+        new Vector2(0f, 1f),
+        new Vector2(1f, 0f),
+        new Vector2(1f, 0f),
+        new Vector2(0f, 1f),
+        new Vector2(1f, 1f)
+    };
+}
