8013804eb6845c7f973090022466382ee1c45bda
IsaacDunn <55945831+IsaacDunn@users.noreply.github.com>
2020-07-20 02:58:11 +0100

    V0.2 Added chunk gen and optimisations
    Added the ability for the game mesh to generate multiple blocks in a chunk.
    Added optimisation to the cube mesh gen to reduce the number of sides drawn.

--- Minecraft clone/Assets/Scripts/Chunk.cs
+++ Minecraft clone/Assets/Scripts/Chunk.cs
@@ -12,24 +12,80 @@ public class Chunk : MonoBehaviour
     List<int> triangles = new List<int>();
     List<Vector2> UVs = new List<Vector2>();
 
+    bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+
     public void Start()
     {
-        AddVoxelDataToChunk(transform.position);
+        PopulateVoxelMap();
+        GenerateChunkMesh();
         GenerateMesh();
+    }
+
+    void GenerateChunkMesh()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+
+    bool CheckVoxel(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+        {
+            return false;
+        }
+
+        return voxelMap[x,y,z];
+    }
+
+    void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+                }
+            }
+        }
     }
 
     void AddVoxelDataToChunk(Vector3 pos)
     {
         for (int p = 0; p < 6; p++)
         {
-            for (int i = 0; i < 6; i++)
+            if (!CheckVoxel(pos + VoxelData.faceChecks[p]))
             {
-                int triangleIndex = VoxelData.voxelTris[p, i];
-                vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
-                triangles.Add(vertexIndex);
-                UVs.Add(VoxelData.voxelUVs[i]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 0]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 1]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 2]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 3]]);
 
-                vertexIndex++;
+                UVs.Add(VoxelData.voxelUVs[0]);
+                UVs.Add(VoxelData.voxelUVs[1]);
+                UVs.Add(VoxelData.voxelUVs[2]);
+                UVs.Add(VoxelData.voxelUVs[3]);
+
+                triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+                vertexIndex += 4;
             }
         }
     }
--- Minecraft clone/Assets/Scripts/VoxelData.cs
+++ Minecraft clone/Assets/Scripts/VoxelData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class VoxelData
 {
+    public static readonly int ChunkWidth = 5;
+    public static readonly int ChunkHeight = 15;
+
     //creates an array of vertices for a cube
     public static readonly Vector3[] voxelVerts = new Vector3[8]
     {
@@ -16,24 +19,33 @@ public class VoxelData
         new Vector3(1f, 1f, 1f),
         new Vector3(0f, 01, 1f)
     };
+
+    public static readonly Vector3[] faceChecks = new Vector3[6]
+    {
+        new Vector3(0f, 0f, -1f),
+        new Vector3(0f, 0f, 1f),
+        new Vector3(0f, 1f, 0f),
+        new Vector3(0f, -1f, 0f),
+        new Vector3(-1f, 0f, 0f),
+        new Vector3(1f, 0f, 0f)
+    };
+
     //creates an array of tris for a cube
-    public static readonly int[,] voxelTris = new int[6,6]{
-        { 0,3,1,1,3,2 }, //back
-        { 5,6,4,4,6,7 }, //front face
-        { 3,7,2,2,7,6 }, //top face
-        { 1,5,0,0,5,4 }, //bottom face
-        { 4,7,0,0,7,3 }, //left face
-        { 1,2,5,5,2,6 } //right face
+    public static readonly int[,] voxelTris = new int[6,4]{
+        { 0,3,1,2 }, //back
+        { 5,6,4,7 }, //front face
+        { 3,7,2,6 }, //top face
+        { 1,5,0,4 }, //bottom face
+        { 4,7,0,3 }, //left face
+        { 1,2,5,6 } //right face
     };
 
     //creates an array of uv co-ords
-    public static readonly Vector2[] voxelUVs = new Vector2[6]
+    public static readonly Vector2[] voxelUVs = new Vector2[4]
     {
         new Vector2(0f, 0f),
         new Vector2(0f, 1f),
         new Vector2(1f, 0f),
-        new Vector2(1f, 0f),
-        new Vector2(0f, 1f),
         new Vector2(1f, 1f)
     };
 }
